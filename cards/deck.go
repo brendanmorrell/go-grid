5701b89e1f1485425d6a797b3aefa2a1c08fd72c
Brendan Morrell <brendanmorrell@gmail.com>
2019-02-14 09:37:22 -0800

    first commit

---
+++ cards/deck.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type deck []string
+type suits []string
+type values []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := suits{"Spades", "Diamond", "Hearts", "Clubs"}
+	cardValues := values{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
